49745d5b90cef46713ac0b711083a06950c511f2
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-26 23:28:34 +0530

    webapplication

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@material-ui/core'
+
+function App() {
+  return (
+    <>
+    <div className="main-div">
+      <h2>this is a web App</h2>
+      
+      <Button variant="contained" color="primary">
+  Primary
+</Button>
+    </div>
+    </>
+  );
+}
+
+export default App;
