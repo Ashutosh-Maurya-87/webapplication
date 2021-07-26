de7a87d5b687035b980d640515d20a2a0ac392e3
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-26 23:51:59 +0530

    webapplication

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core'
+import { Button, Link } from '@material-ui/core'
 
 function App() {
   return (
     <>
     <div className="main-div">
       <div className='nav-div'>
-      <ul>
-        <li>NavBar</li>
-        <li>NavBar</li>
-        <li>NavBar</li>
-        <li>NavBar</li>
-      </ul>
+      <Link href="#" >Home</Link>
+      <Link href="#" >Feedback</Link>
+      <Link href="#" >FAQ</Link>
+      <Link href="#" >Contact</Link>
       </div>
       <Button variant="contained" color="primary">
   Primary
