98d43de00385957710549cd66bf842fc4ba1d28f
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-28 20:52:54 +0530

    jsfileupdate

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button, Link } from '@material-ui/core'
-import CompA from './CompA';
+import CompC from './CompC';
 import { createContext } from 'react';
 
-const FirstName = createContext ();
-const LastName = createContext ();
+const FirstName = createContext();
+const LastName = createContext();
 
-const App =()=> {
-  
+const App = () => {
+
   return (
     <>
-    {/* <div className="main-div">
-      <div className='nav-div'>
-      <Link href="#" >Home</Link>
-      <Link href="#" >Feedback</Link>
-      <Link href="#" >FAQ</Link>
-      <Link href="#" >Contact</Link>
-      </div>
-      <Button variant="contained" color="primary">
-  Primary
-</Button> */}
-<FirstName.Provider value="AShu">
-  <LastName.Provider value="Maurya">
-<CompA />
-</LastName.Provider>
-</FirstName.Provider>
-     
-    {/* </div> */}
+      <div className="main-div">
+        <div className='nav-div'>
+          <Link href="#" >Home</Link>
+          <Link href="#" >Feedback</Link>
+          <Link href="#" >FAQ</Link>
+          <Link href="#" >Contact</Link>
+        </div>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <FirstName.Provider value="AShu">
+          <LastName.Provider value="Maurya">
+            <CompC />
+          </LastName.Provider>
+        </FirstName.Provider>
+
+        </div>
     </>
-  );
+      );
 }
 
-export default App;
-export { FirstName , LastName} ;
+      export default App;
+      export {FirstName, LastName} ;
